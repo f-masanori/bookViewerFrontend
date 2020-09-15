73decbcb8ac058df2bae036a5aafc5f63302f27c
Keitaro <keitaro.cryptocurrency@gmail.com>
2020-09-15 19:24:29 +0900

    質問詳細モデル、質問リストモデル、回答モデルを追加

--- src/services/forViewer/models.ts
+++ src/services/forViewer/models.ts
@@ -11,3 +11,41 @@ export interface BookForViewer {
     }[];
   }[];
 }
+
+// 共通する部分は合体できる？
+// 質問する場合はuserIdを送信するようになっている
+export interface Question {
+  userName: string;
+  createdAt: string;
+  pageNum: number;
+  rowNum: number;
+  title: string;
+  content: string;
+}
+
+export interface QuestionList {
+  questions: {
+    qustionId: number;
+    userName: string;
+    title: string;
+    createdAt: string;
+  }[];
+}
+
+// 回答のモデルはまとめて、読者と著者はisAuthorやuserName等で判断するのもあり？
+export interface AnswersFromAuthor {
+  answers: {
+    createdAt: string;
+    content: string;
+    likeNum: number;
+  }[];
+}
+
+export interface AnswersFromReader {
+  answers: {
+    userNmae: string;
+    createdAt: string;
+    content: string;
+    likeNum: number;
+  }[];
+}
