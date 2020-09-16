424fbd0fdb4020ec3dd48b96c08f9951a0e45107
f-masanori <pmasanori0306@gmail.com>
2020-09-16 10:37:16 +0900

    viewerのフレーム作成

--- src/component/organisms/header.tsx
+++ src/component/organisms/header.tsx
@@ -1,11 +1,68 @@
 import React from 'react';
-import { Header, Segment } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import { Menu, Button, Modal, Form, Input, TextArea } from 'semantic-ui-react';
 
-const ViewerHeader = (): JSX.Element => {
+const ViewerHeader = (setVisible: any): JSX.Element => {
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <Segment>
-      <Header textAlign="right">Float Right</Header>
-    </Segment>
+    <div>
+      <Menu>
+        <Menu.Item name="MyPage" />
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Modal
+              onClose={() => setOpen(false)}
+              onOpen={() => setOpen(true)}
+              open={open}
+              trigger={<Button>質問する</Button>}
+            >
+              <Modal.Header>質問投稿フォーム</Modal.Header>
+              <Modal.Content>
+                <Modal.Description>
+                  <Form>
+                    <Form.Group widths="equal">
+                      <Form.Field
+                        control={Input}
+                        label="ページ番号"
+                        placeholder="ページ番号"
+                      />
+                      <Form.Field
+                        control={Input}
+                        label="文章の先頭行番号"
+                        placeholder="文章の先頭行番号"
+                      />
+                    </Form.Group>
+                    <Form.Field
+                      control={TextArea}
+                      label="質問タイトル"
+                      placeholder="質問タイトルをご記入下さい"
+                    />
+                    <Form.Field
+                      control={TextArea}
+                      label="質問内容"
+                      placeholder="質問内容をご記入下さい"
+                    />
+                  </Form>
+                </Modal.Description>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button color="black" onClick={() => setOpen(false)}>
+                  破棄
+                </Button>
+                <Button color="green" onClick={() => setOpen(false)}>
+                  投稿
+                </Button>
+              </Modal.Actions>
+            </Modal>
+          </Menu.Item>
+          <Menu.Item>
+            <Button onClick={() => setVisible(true)}> 質問リスト</Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    </div>
   );
 };
+
 export default ViewerHeader;
