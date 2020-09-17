c56d3ecd521a2f19b31a06dad0d1bfaf6acc72ba
Keitaro <keitaro.cryptocurrency@gmail.com>
2020-09-17 16:11:16 +0900

    質問詳細UI修正(tabの文字が黒くならない)

--- src/component/organisms/detailQuestion.tsx
+++ src/component/organisms/detailQuestion.tsx
@@ -1,32 +1,53 @@
 import React, { useState } from 'react';
-import { Grid, Item, Image, Menu, Button, Tab } from 'semantic-ui-react';
+import { useDispatch } from 'react-redux';
+import {
+  Item,
+  Image,
+  Menu,
+  Button,
+  Tab,
+} from 'semantic-ui-react';
+import { ConbineState } from '../../reducer/index';
 import { AnswerForm } from '../molecules/answerForm';
 import { CorrespondingPages } from './correspondingPages';
 
-export const DetailQuestion: React.FC<any> = (): JSX.Element => {
+export const DetailQuestion: React.FC<any> = (bookQuestionState): JSX.Element => {
   const [visible, setVisible] = useState(false);
+
+  const detailQustion = bookQuestionState.bookDetailQuestion.detailQuestion;
+  const answerFromAuthor = bookQuestionState.bookDetailQuestion.answersFromAuthor;
+  const answerFromReader = bookQuestionState.bookDetailQuestion.answersFromReader;
+
   const panes = [
     {
       menuItem: '著者の回答',
       render: () => (
         <Tab.Pane>
-          <Item.Group>
-            <Item>
-              <Item.Image
+          {(() => {
+            if (answerFromAuthor) {
+              return (
+                <Item.Group>
+                  <Item>
+                    {/* <Item.Image
                 size="tiny"
                 src="https://react.semantic-ui.com/images/wireframe/image.png"
-              />
+              /> */}
 
-              <Item.Content>
-                <Item.Header>Header</Item.Header>
-                <Item.Meta>Description</Item.Meta>
-                <Item.Description>
-                  <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
-                </Item.Description>
-                <Item.Extra>Additional Details</Item.Extra>
-              </Item.Content>
-            </Item>
-          </Item.Group>
+                    <Item.Content>
+                      <Item.Header>Header</Item.Header>
+                      <Item.Meta>Description</Item.Meta>
+                      <Item.Description>
+                        <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+                      </Item.Description>
+                      <Item.Extra>Additional Details</Item.Extra>
+                    </Item.Content>
+                  </Item>
+                </Item.Group>
+              );
+            } else {
+              return <p>回答なし</p>;
+            }
+          })()}
         </Tab.Pane>
       ),
     },
@@ -35,25 +56,29 @@ export const DetailQuestion: React.FC<any> = (): JSX.Element => {
       render: () => (
         <Tab.Pane>
           {(() => {
-            return (
-              <Item.Group>
-                <Item>
-                  <Item.Image
-                    size="tiny"
-                    src="https://react.semantic-ui.com/images/wireframe/image.png"
-                  />
+            if (answerFromReader) {
+              return <p>回答なし</p>;
+            } else {
+              return (
+                <Item.Group>
+                  <Item>
+                    {/* <Item.Image
+                size="tiny"
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+              /> */}
 
-                  <Item.Content>
-                    <Item.Header>Header</Item.Header>
-                    <Item.Meta>Description</Item.Meta>
-                    <Item.Description>
-                      <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
-                    </Item.Description>
-                    <Item.Extra>Additional Details</Item.Extra>
-                  </Item.Content>
-                </Item>
-              </Item.Group>
-            );
+                    <Item.Content>
+                      <Item.Header>Header</Item.Header>
+                      <Item.Meta>Description</Item.Meta>
+                      <Item.Description>
+                        <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+                      </Item.Description>
+                      <Item.Extra>Additional Details</Item.Extra>
+                    </Item.Content>
+                  </Item>
+                </Item.Group>
+              );
+            }
           })()}
         </Tab.Pane>
       ),
@@ -61,37 +86,38 @@ export const DetailQuestion: React.FC<any> = (): JSX.Element => {
   ];
 
   return (
-    <Grid columns={2} divided>
-      <Grid.Row>
-        <Grid.Column />
-        <Grid.Column>
-          <Menu>
-            <Menu.Item>
-              <Button icon="left arrow" />
-            </Menu.Item>
-            <Menu.Item>1章質問詳細</Menu.Item>
-          </Menu>
-          <Item.Group>
-            <Item>
-              <Item.Image
-                size="tiny"
-                src="https://react.semantic-ui.com/images/wireframe/image.png"
-              />
+    <div style={{ backgroundColor: '#FFFFFF' }}>
+      <Menu>
+        <Menu.Item>
+          <Button icon="left arrow" />
+        </Menu.Item>
+        <Menu.Item>1章質問詳細</Menu.Item>
+      </Menu>
+      <Item.Group style={{ padding: '10px' }}>
+        <Item>
+          <Item.Image
+            size="tiny"
+            src="https://react.semantic-ui.com/images/wireframe/image.png"
+          />
 
-              <Item.Content>
-                <Item.Header>userName</Item.Header>
-                <Item.Description>質問内容</Item.Description>
-                <Item.Extra>createdAt</Item.Extra>
-              </Item.Content>
-            </Item>
-            <Item>
-              <CorrespondingPages contents={['fjas', 'fjio']} />
-              <AnswerForm />
-            </Item>
-          </Item.Group>
-          <Tab panes={panes} />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+          <Item.Content>
+            <Item.Header>Header</Item.Header>
+            <Item.Meta>Description</Item.Meta>
+            <Item.Description>
+              <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+            </Item.Description>
+            <Item.Extra>Additional Details</Item.Extra>
+          </Item.Content>
+        </Item>
+      </Item.Group>
+      <div>
+        <CorrespondingPages contents={['fjas', 'fjio']} />
+        <AnswerForm />
+      </div>
+
+      <div style={{ color: 'black', padding: '10px' }}>
+        <Tab panes={panes} />
+      </div>
+    </div>
   );
 };
