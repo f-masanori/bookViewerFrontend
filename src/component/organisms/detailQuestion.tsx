7ca475378ed91aada3b3de9fbec926fd9e92cc97
Keitaro <keitaro.cryptocurrency@gmail.com>
2020-09-18 04:36:35 +0900

    apiで得た質問詳細情報を質問詳細ページに表示

--- src/component/organisms/detailQuestion.tsx
+++ src/component/organisms/detailQuestion.tsx
@@ -24,6 +24,7 @@ export const DetailQuestion: React.FC<any> = (
   const [visible, setVisible] = useState(false);
   const reduxState = useSelector((state: ConbineState) => state);
   const answers = reduxState.answers;
+  const detailQuestions = reduxState.detailQuestions.detailQuestions;
   const dispatch = useDispatch();
 
   const panes = [
@@ -140,15 +141,15 @@ export const DetailQuestion: React.FC<any> = (
           }}
         >
           <Card.Content>
-            <Card.Header>質問タイトル</Card.Header>
+            <Card.Header>{detailQuestions.title}</Card.Header>
           </Card.Content>
           <Card.Content>
             <Feed>
               <Feed.Event>
                 <Feed.Label image="https://drive.google.com/uc?id=1H6_sJtNlwBIcRT4F6oDNY-1Eq4L-JiB3" />
                 <Feed.Content>
-                  <Feed.Date content="createdAt" />
-                  <Feed.Summary>質問内容をここに書いてください</Feed.Summary>
+                  <Feed.Date content={detailQuestions.createdAt} />
+                  <Feed.Summary>{detailQuestions.content}</Feed.Summary>
                 </Feed.Content>
               </Feed.Event>
             </Feed>
