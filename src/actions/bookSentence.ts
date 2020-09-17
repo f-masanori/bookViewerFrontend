add171932c3ca7bb87a3d284a874b6bce06be6bc
kanekou <e165726@ie.u-ryukyu.ac.jp>
2020-09-17 20:11:20 +0900

    add actions

---
+++ src/actions/bookSentence.ts
@@ -0,0 +1,34 @@
+import { AxiosError } from 'axios';
+
+import { BookPage } from '../services/forViewer/models';
+import * as ActionType from './bookSentenceContents';
+
+interface GetBookPageParams {
+  bookID: number;
+}
+interface GetBookPageResult {
+  bookSentence: BookPage;
+}
+
+export const getBookPage = {
+  start: (params: GetBookPageParams) => ({
+    type: ActionType.GET_BOOK_SENTENCE_LIST_START,
+    payload: params,
+  }),
+
+  succeed: (params: GetBookPageParams, result: GetBookPageResult) => ({
+    type: ActionType.GET_BOOK_SENTENCE_LIST_SUCCEED,
+    payload: { params, result },
+  }),
+
+  fail: (params: GetBookPageParams, error: AxiosError) => ({
+    type: ActionType.GET_BOOK_SENTENCE_LIST_FAIL,
+    payload: { params, error },
+    error: true,
+  }),
+};
+
+export type BookPageAction =
+  | ReturnType<typeof getBookPage.start>
+  | ReturnType<typeof getBookPage.succeed>
+  | ReturnType<typeof getBookPage.fail>;
